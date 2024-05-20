75382141728a2b3e4843c260345fe1d09f85bfd0
LingshijunRenzy <598364536@qq.com>
2024-05-20 09:06:12 +0000

    feat: Add UserDao interface and UserDaoImpl implementation

--- src/main/java/org/tomjerry/sweethome/dao/UserDao.java
+++ src/main/java/org/tomjerry/sweethome/dao/UserDao.java
@@ -1,4 +1,11 @@
 package org.tomjerry.sweethome.dao;
 
+import org.tomjerry.sweethome.entity.UserEntity;
+
 public interface UserDao{
+    public UserEntity getUserById(int id);
+    public UserEntity getUserByName(String name);
+    public void addUser(UserEntity user);
+    public void updateUser(UserEntity user);
+    public void deleteUser(int id);
 }
---
+++ src/main/java/org/tomjerry/sweethome/dao/userImpl/UserDaoImpl.java
@@ -0,0 +1,25 @@
+package org.tomjerry.sweethome.dao.userImpl;
+
+import org.tomjerry.sweethome.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    public UserEntity getUserById(int id) {
+        // implementation
+    }
+
+    public UserEntity getUserByName(String name) {
+        // implementation
+    }
+
+    public void addUser(UserEntity user) {
+        // implementation
+    }
+
+    public void updateUser(UserEntity user) {
+        // implementation
+    }
+
+    public void deleteUser(int id) {
+        // implementation
+    }
+}
